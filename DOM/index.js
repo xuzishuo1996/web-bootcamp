959d2d080eada559684c161c6233c93d09f31779
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-09 17:43:31 +0800

    DOM/: separate style and behav: use .classList.add('xxx') instead of .style.xxx = 'xxx'.

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,5 @@
-document.querySelector("li a").style.color = "yellow";
 document.querySelector("h1").innerHTML = "Good Bye";
-document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("li a").style.color = "yellow";  //bad style
+document.querySelector("button").style.backgroundColor = "yellow";  //bad style
+document.querySelector("button").classList.add("invisible");  //good style
+document.querySelector("h1").classList.add("huge"); //good style
