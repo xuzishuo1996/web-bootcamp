05387c6d4ffe6fd63f2abfea8a35884ecfc41897
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-11 14:49:20 +0800

    Drum_Kit/: added keydown listener to play sound.

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -1,35 +1,45 @@
 var numOfDrumButtons = document.querySelectorAll(".drum").length;
 
+//Detecting Button Press
 for (var i = 0; i < numOfDrumButtons; ++i) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    var filename;
-    switch (this.innerHTML) {
-      case "w":
-        filename = "tom-1";
-        break;
-      case "a":
-        filename = "tom-2";
-        break;
-      case "s":
-        filename = "tom-3";
-        break;
-      case "d":
-        filename = "tom-4";
-        break;
-      case "j":
-        filename = "snare";
-        break;
-      case "k":
-        filename = "crash";
-        break;
-      case "l":
-        filename = "kick-bass";
-        break;
-      default:
-        console.log(this.innerHTML);
-    }
-
-    var audio = new Audio("sounds/" + filename + ".mp3");
-    audio.play();
+    makeSound(this.innerHTML);
   });
+}
+
+//Detecting Keyboard Press
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+})
+
+function makeSound(key) {
+  var filename;
+  switch (key) {
+    case "w":
+      filename = "tom-1";
+      break;
+    case "a":
+      filename = "tom-2";
+      break;
+    case "s":
+      filename = "tom-3";
+      break;
+    case "d":
+      filename = "tom-4";
+      break;
+    case "j":
+      filename = "snare";
+      break;
+    case "k":
+      filename = "crash";
+      break;
+    case "l":
+      filename = "kick-bass";
+      break;
+    default:
+      console.log(this.innerHTML);
+  }
+
+  var audio = new Audio("sounds/" + filename + ".mp3");
+  audio.play();
 }
