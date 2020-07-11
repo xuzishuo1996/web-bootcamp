5f2cab5a295fe1a8179c041e26697e7ae95c5171
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-11 14:29:28 +0800

    Drum_Kit/: added sound to buttons.

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -1,7 +1,35 @@
 var numOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numOfDrumButtons; ++i) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var filename;
+    switch (this.innerHTML) {
+      case "w":
+        filename = "tom-1";
+        break;
+      case "a":
+        filename = "tom-2";
+        break;
+      case "s":
+        filename = "tom-3";
+        break;
+      case "d":
+        filename = "tom-4";
+        break;
+      case "j":
+        filename = "snare";
+        break;
+      case "k":
+        filename = "crash";
+        break;
+      case "l":
+        filename = "kick-bass";
+        break;
+      default:
+        console.log(this.innerHTML);
+    }
+
+    var audio = new Audio("sounds/" + filename + ".mp3");
+    audio.play();
   });
 }
