d1656d4f99ff890404a5c7e1c7656c3a6c03f901
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-11 15:07:15 +0800

    Drum_Kit/: added animation using setTimeout function.

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -3,13 +3,15 @@ var numOfDrumButtons = document.querySelectorAll(".drum").length;
 //Detecting Button Press
 for (var i = 0; i < numOfDrumButtons; ++i) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    makeSound(this.innerHTML);
+    makeSound(this.innerHTML);  //currentKey
+    buttonAnimation(this.innerHTML);
   });
 }
 
 //Detecting Keyboard Press
 document.addEventListener("keypress", function(event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 })
 
 function makeSound(key) {
@@ -42,4 +44,12 @@ function makeSound(key) {
 
   var audio = new Audio("sounds/" + filename + ".mp3");
   audio.play();
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");  //not .pressed
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 1000);
 }
