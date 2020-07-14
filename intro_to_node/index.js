970730f37a2f05db25d9dae080888b0345c3b036
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-14 12:44:54 +0800

    intro_to_node/: learned node and npm.

---
+++ intro_to_node/index.js
@@ -0,0 +1,6 @@
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+var superheroes = require("superheroes");
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
