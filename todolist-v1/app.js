912285b7853ebe9ff7934c64ca9e7d02251422c8
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-17 12:12:30 +0800

    todolist-v1/: pass variable to EJS templates.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,18 +4,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-
-var today = new Date();
-var currentDay = today.getDay();
+app.set("view engine", "ejs"); //cannot use app.use, follow https://ejs.co
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", (req, res) => {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.send("<h1>Yay it's the weekend<h1>");
-  } else {
-    res.sendFile(__dirname + "/index.html");
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
   }
+  res.render("list", {
+    kindofDay: day
+  });
 });
 
 app.listen(3000, () => {
