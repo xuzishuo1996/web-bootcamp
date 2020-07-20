c5130c3d2db0aedb95c4711533471d7f852cad3e
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-20 15:08:57 +0800

    Secrets/: authentication level 4 - hash password with salt (round = 10) using bcrypt npm package

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -24,7 +26,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-// encrypt by a single string instead of two keys
+// Level 2: encrypt by a single string instead of two keys using mongoose-encryption
 // userSchema.plugin(encrypt, {secret: process.env.API_KEY, encryptedFields: ["password"]});
 
 //create a collection: users
@@ -44,34 +46,57 @@ app.get("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
+  // for md5 only
+  // const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+        // if (foundUser.password === password) {
+        //   res.render("secrets");
+        // }
       }
     }
   });
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+
+  // Level 4: bcrypt with salt
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");  //secrets.ejs
+      }
+    });
   });
 
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");  //secrets.ejs
-    }
-  });
+  // Level 3: md5 hash
+  // const newUser = new User({
+  //   email: req.body.username,
+  //   password: md5(req.body.password)
+  // });
+  // newUser.save((err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     res.render("secrets");  //secrets.ejs
+  //   }
+  // });
 });
 
 app.listen(3000, () => {
